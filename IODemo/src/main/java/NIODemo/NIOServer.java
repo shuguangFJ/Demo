8d334c4c3dd7c168872d6e80e2cc4dcc857733b2
fengjie <937921918@qq.com>
2022-04-20 17:45:31 +0800

    NIO,Selector,Channel,Buffer

--- IODemo/src/main/java/NIODemo/NIOServer.java
+++ IODemo/src/main/java/NIODemo/NIOServer.java
@@ -1,17 +1,55 @@
 package NIODemo;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.Charset;
+import java.util.Iterator;
+import java.util.Set;
 
 public class NIOServer extends Thread {
 
     @Override
     public void run() {
-        try (Selector selector = Selector.open()) {
-
+        try (Selector selector = Selector.open(); ServerSocketChannel serverSocketChannel = ServerSocketChannel.open()) {
+            serverSocketChannel.bind(new InetSocketAddress(InetAddress.getLocalHost(), 9876));
+            serverSocketChannel.configureBlocking(false);
+            serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+            while (true) {
+                selector.select();
+                Set<SelectionKey> selectionKeys = selector.selectedKeys();
+                Iterator<SelectionKey> iterator = selectionKeys.iterator();
+                if (iterator.hasNext()) {
+                    SelectionKey next = iterator.next();
+                    sayHello((ServerSocketChannel)next.channel());
+                    iterator.remove();
+                }
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private void sayHello(ServerSocketChannel channel) throws IOException {
+        try (SocketChannel accept = channel.accept()) {
+            accept.write(Charset.defaultCharset().encode("hello world!!!"));
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        NIOServer nioServer = new NIOServer();
+        nioServer.start();
+
+        Socket socket = new Socket(InetAddress.getLocalHost(),9876);
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        bufferedReader.lines().forEach(l-> System.out.println(l));
     }
 }
